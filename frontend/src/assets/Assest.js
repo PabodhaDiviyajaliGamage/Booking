409cfda6431c3fc996c9230b1eb498637a8825c7
PabodhaDiviyajaliGamage <cbuilderit@gmail.com>
2025-11-02 14:21:18 +0530

    feat: add frontend assets with Git LFS

--- frontend/src/assets/Assest.js
+++ frontend/src/assets/Assest.js
@@ -1,11 +1,3 @@
-import logo from './logo.png';
-import Hero from './Hero.jpg';
-import contact from './contact.jpg';
-
-
-export const assets={
-    logo,
-    Hero,
-    contact
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:5852d04b6624b41a314803d319acac139b83f0c394f2bc09bcfe2b0787dbc114
+size 158
